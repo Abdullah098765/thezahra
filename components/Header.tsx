9a07b636d567902ab99d5b625ba7108b664dba13
AbdullahSabir098765 <ssmmhazz@gmail.com>
2024-08-31 13:34:06 +0500

    logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import {MouseEvent} from 'react';
 import Link from 'next/link';
 import HeaderCart from './cart/HeaderCart';
 import ChooseVariantModal from './header/ChooseVariantModal';
-import logoImg from '../assets/logo.png';
+import logoImg from '../assets/logo.svg';
 import {faBars} from '@fortawesome/free-solid-svg-icons/faBars';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {useAppDispatch} from '../hooks/redux';
